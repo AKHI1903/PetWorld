0f762ed98dee7971a71eb10544a9c0b2a75da0e8
AKHI1903 <akhip1903@gmail.com>
2025-08-26 14:22:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Sample Data
+// Data
 const pets = [
     {
         id: 1,
@@ -71,6 +71,102 @@ const pets = [
         description: "Energetic Beagle with excellent temperament. Loves to play and explore.",
         image: "https://th.bing.com/th/id/R.a576c93c95718d7da589056b353ae69f?rik=YYBPN4zfS8ISoA&riu=http%3a%2f%2f3.bp.blogspot.com%2f-TGbPfC9vxg8%2fUAa-P3trzAI%2fAAAAAAAAAH0%2fYKd_ai8tKk0%2fs1600%2fBeagle-Dog-Breed-Pic.jpg&ehk=O6ltQ3%2fgPKQQazbVxCJ0%2bstKMXIY81Xzndh7%2fk1zwJw%3d&risl=&pid=ImgRaw&r=0",
         status: "available"
+    },
+    {
+        id: 7,
+        name: "snoopy",
+        breed: "Pug",
+        type: "dog",
+        age: "6 months",
+        gender: "male",
+        price: 32000,
+        description: "Cute,small personality with a wrinkled face.",
+        image: "https://wallpapers.com/images/hd/pug-t7s7uobo475tjhg9.jpg",
+        status: "available"
+    },
+    {
+        id: 8,
+        name: "Simba",
+        breed: "Maine Coon",
+        type: "cat",
+        age: "8 months",
+        gender: "male",
+        price: 80000,
+        description: "Large,Gentle.",
+        image: "https://framerusercontent.com/images/T8je8KOib5f8aE1iKk0icDxALU.jpeg?width=1000&height=1052",
+        status: "available"
+    },
+    {
+        id: 9,
+        name: "Tiger",
+        breed: "Bengal Cat",
+        type: "cat",
+        age: "5 months",
+        gender: "male",
+        price: 25000,
+        description: "Exotic,active,leopard-like spots.",
+        image: "https://spotpetinsurance.ca/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fm5ehn3s5t7ec%2Fwp-image-197606%2F673d0bc11558a640ec3e388c50770636%2FHow-Much-is-a-Bengal-Cat.webp&w=3840&q=75",
+        status: "available"
+    },
+    {
+        id: 10,
+        name: "Tank",
+        breed: "American Bull Dog",
+        type: "dog",
+        age: "12 months",
+        gender: "male",
+        price: 87000,
+        description: "Calm,Strong,yet affectionate.",
+        image: "https://media.istockphoto.com/id/1255915151/photo/a-white-american-bulldog-mixed-breed-dog-with-brown-spots.jpg?s=612x612&w=0&k=20&c=axwlRQyzjovlocZrmzGzbBB-y_TBJWb-C3SdzyzV3l4=",
+        status: "available"
+    },
+    {
+        id: 11,
+        name: "Milo",
+        breed: "Ragdoll Cat",
+        type: "cat",
+        age: "7 months",
+        gender: "male",
+        price: 14000,
+        description: "Relaxed,cuddly,family-friendly.",
+        image: "https://cdn.mos.cms.futurecdn.net/v2/t:0,l:420,cw:1080,ch:1080,q:80,w:1080/PAyDK8eyvNTtwHXHeWTiiD.jpg",
+        status: "available"
+    },
+    {
+        id: 11,
+        name: "Snowy",
+        breed: "Pomeranian",
+        type: "dog",
+        age: " 3 months",
+        gender: "male",
+        price: 4000,
+        description: "Small,fluffy,and lively.",
+        image: "https://cdn-ilbfemn.nitrocdn.com/SMbNqQVgokVBOKPHqgPbhvowhPKWFJah/assets/images/optimized/rev-f1db8fa/canna-pet.com/wp-content/uploads/2017/09/common-pomeranian-health-issues_canna-pet-1024x683.jpg",
+        status: "available"
+    },
+     {
+        id: 12,
+        name: "Mimi",
+        breed: "Scottish Fold",
+        type: "cat",
+        age: "3 months",
+        gender: "male",
+        price: 2000,
+        description: "Folded ears,calm and sweet.",
+        image: "https://profile.perfectpets.au/2987/19017275835e9af9a89356e3.62368203-profile-2987.jpeg",
+        status: "available"
+    },
+    {
+        id: 13,
+        name: "Sky",
+        breed: "Siberian Husky",
+        type: "dog",
+        age: "14 months",
+        gender: "male",
+        price: 60000,
+        description: "Energetic,striking blue eyes,loves cold.",
+        image: "https://i.pinimg.com/736x/26/4b/94/264b94b898ca630d800bd0c798cc4236.jpg",
+        status: "available"
     }
 ];
 
@@ -79,13 +175,40 @@ const products = [
         id: 1,
         name: "Royal Canin Adult Dog Food",
         description: "Premium nutrition for adult dogs. Complete and balanced diet.",
-        price: 2850,
+        price: 1050,
         category: "food",
         petType: "dog",
         image: "https://images.unsplash.com/photo-1589924691995-400dc9ecc119?w=400&h=300&fit=crop"
     },
     {
         id: 2,
+        name: "Biscuits & Cookies",
+        description: "Crunchy treats for training and rewards.",
+        price: 800,
+        category: "food",
+        petType: "dog",
+        image: "https://www.thecookierookie.com/wp-content/uploads/2013/12/dog-treat-recipe-7.jpg"
+    },
+    {
+        id: 3,
+        name: "Dry Kibble",
+        description: "Crunchy pellets,easy to store.",
+        price: 650,
+        category: "food",
+        petType: "cat",
+        image: "https://media.istockphoto.com/id/1131841641/photo/dry-pet-food.jpg?s=612x612&w=0&k=20&c=zNKlNVZ-gCYC-b6VUjfrardCC0bYkfPk-C3qlnFxfts="
+    },
+    {
+        id: 4,
+        name: "Kitten Formula",
+        description: "Extra nutrition for fast-growing kittens.",
+        price: 230,
+        category: "food",
+        petType: "cat",
+        image: "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/78bd9f8f12ab4e5c89a9d7b13415e2c6.jpg?dpr=2&format=auto",
+    },
+    {
+        id: 5,
         name: "Interactive Cat Toy Bundle",
         description: "Set of 5 interactive toys to keep your cat entertained and active.",
         price: 1200,
@@ -94,7 +217,7 @@ const products = [
         image: "https://tse3.mm.bing.net/th/id/OIP.jhQUfrTXQbLdg6fba_rYJAHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
     },
     {
-        id: 3,
+        id: 6,
         name: "Premium Leather Collar Set",
         description: "High-quality leather collar with matching leash for dogs.",
         price: 950,
@@ -119,7 +242,43 @@ const products = [
         category: "accessories",
         petType: "both",
         image: "https://th.bing.com/th/id/OIP.t5rEmoCMpBL8nhhGC2Z23QHaE8?w=297&h=198&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3"
-    }
+    },
+    {
+        id: 9,
+        name: "Dog Beds",
+        description: "Soft & cozy beds for comforrtable sleep.",
+        price: 450,
+        category: "accessories",
+        petType: "both",
+        image: "https://m.media-amazon.com/images/I/91dC4aGb3NL._UF1000,1000_QL80_.jpg"
+    },
+    {
+        id: 10,
+        name: "Scratching Posts & Towers",
+        description: "Essential for scratching and climbing.",
+        price: 2499,
+        category: "accessories",
+        petType: "cat",
+        image: "https://media.very.co.uk/i/very/VHYKV_SQ7_0000000099_N_A_SLd5?$pdp_300x400_x2$&fmt=jpg"
+    },
+    {
+        id: 11,
+        name: "Chew Toys",
+        description: "Durable rubber toys for teething & Dental health.",
+        price: 300,
+        category: "toys",
+        petType: "dog",
+        image: "https://kutkutstyle.com/cdn/shop/files/kutkutstyle-teether-blue-kutkut-dog-chew-toys-for-aggressive-chewers-large-breed-non-toxic-natural-rubber-indestructible-dog-toys-tough-durable-puppy-chew-toy-for-medium-large-dogs-fu_1de9b293-908e-4415-932b-46ef2354fb35.jpg?v=1753373912"
+    },
+    {
+        id: 12,
+        name: "Laser Pointers",
+        description: "Keeps cats active with endless chasing fun.",
+        price: 150,
+        category: "toys",
+        petType: "cat",
+        image: "https://www.dailypaws.com/thmb/aBjoXcqs0Psms1JYVW6izQwNs8c=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/are-laser-pointers-bad-for-cats-1249980070-2000-12e44904760d4bf7bc3c17588e210347.jpg"
+    },
 ];
 
 // Cart functionality
@@ -364,7 +523,7 @@ function checkout() {
     }
     
     const total = cart.reduce((sum, item) => sum + (item.price * item.quantity), 0);
-    showToast(`Order placed for ${formatPrice(total)}! Thank you for shopping with PawMart.`);
+    showToast(`Order placed for ${formatPrice(total)}! Thank you for shopping with PetWorld.`);
     
     // Clear cart
     cart = [];
